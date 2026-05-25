e726295e8dc8c954283084fc52534aafa9fd52e6
Donovan <Donovangriffin04@gmail.com>
2026-05-25 07:42:20 -0700

    File path validation

--- RemoveOldLogs.ps1
+++ RemoveOldLogs.ps1
@@ -11,11 +11,26 @@ function remove-oldlogs {
     begin { #Caluculates cutoff date
         $cutOffDate = (Get-Date).AddDays(-$daysold)
         Write-Host "Cutoff date it {$cutOffDate}"
+
     }
 
     process{
-        $logArr = Get-ChildItem $logpath -Filter "*.log" #creates and array with all log files in the specified path
-        
+        $logArr = @()
+        try{
+            Get-Item $logpath -ErrorAction Stop
+            $logArr = Get-ChildItem $logpath -Filter "*.log" #creates and array with all log files in the specified path
+
+        }
+        catch [System.Management.Automation.ItemNotFoundException] {
+            Write-Warning "$logpath not found, Please rerun the command and enter a valid path"
+            return
+        }
+        catch {
+            write-warning "An error occured"
+            Write-Error $_
+            return 
+        }
+
         foreach ($log in $logArr){
             Write-Host "Checking date for {$log}"
             if($log.LastWriteTime -lt $cutOffDate){ #Checks if the last write time of the log file is older than the cutoff date
